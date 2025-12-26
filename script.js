3d6a42bb2123834e3f2fce2b6eac5ae18c78dbd5
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-12-26 11:38:56 +0800

    A

---
+++ script.js
@@ -0,0 +1,90 @@
+const cardElements = document.getElementsByClassName("card");
+const iconElements = document.getElementsByClassName("icons");
+
+const content = [
+  {
+    id: 1,
+    color: `green;`,
+    img: `url(icons/icons8-chatgpt-48.png);`,
+  },
+  {
+    id: 2,
+    color: `rgb(42, 37, 65);`,
+    img: `url(icons/icons8-discord-48.png);`,
+  },
+  {
+    id: 3,
+    color: ` rgb(73, 73, 255);`,
+    img: `url(icons/icons8-facebook-circled-48.png);`,
+  },
+  {
+    id: 4,
+    color: `rgb(32, 32, 32);`,
+    img: `url(icons/icons8-github-48.png);`,
+  },
+  {
+    id: 5,
+    color: `rgb(141, 149, 0);`,
+    img: `url(icons/icons8-google-48.png);`,
+  },
+  {
+    id: 6,
+    color: `rgb(255, 73, 103);`,
+    img: `url(icons/icons8-instagram-48.png);`,
+  },
+  {
+    id: 7,
+    color: `black`,
+    img: `url(icons/icons8-twitter-bird-48.png);`,
+  },
+  {
+    id: 8,
+    color: `red`,
+    img: `url(icons/icons8-youtube-48.png);`,
+  },
+  {
+    id: 1,
+    color: `green;`,
+    img: `url(icons/icons8-chatgpt-48.png);`,
+  },
+  {
+    id: 2,
+    color: `rgb(42, 37, 65);`,
+    img: `url(icons/icons8-discord-48.png);`,
+  },
+  {
+    id: 3,
+    color: ` rgb(73, 73, 255);`,
+    img: `url(icons/icons8-facebook-circled-48.png);`,
+  },
+  {
+    id: 4,
+    color: `rgb(32, 32, 32);`,
+    img: `url(icons/icons8-github-48.png);`,
+  },
+  {
+    id: 5,
+    color: `rgb(141, 149, 0);`,
+    img: `url(icons/icons8-google-48.png);`,
+  },
+  {
+    id: 6,
+    color: `rgb(255, 73, 103);`,
+    img: `url(icons/icons8-instagram-48.png);`,
+  },
+  {
+    id: 7,
+    color: `black`,
+    img: `url(icons/icons8-twitter-bird-48.png);`,
+  },
+  {
+    id: 8,
+    color: `red`,
+    img: `url(icons/icons8-youtube-48.png);`,
+  },
+];
+
+// for (let i = 0; i < cardElements.length; i++) {
+//   const cardElement = cardElements[i];
+//   cardElement.addEventListener(`click`, console.log(cardElement));
+// }
